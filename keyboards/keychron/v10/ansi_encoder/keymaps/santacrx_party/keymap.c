5dc27369114b48acc51c06a882f325b24bcabf38
santacrx <santacrx@gmail.com>
2024-04-11 15:42:03 -0700

    first attempt at working party mode. can't get it to compile. rgb_matrix_user.inc fails as it's looking for global functions it can't find

--- keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_party/keymap.c
+++ keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_party/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN1] = LAYOUT_ansi_89(
         KC_MUTE,  KC_CALC,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_PGDN,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_PGDN,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                      _______,  _______,  _______),
 
@@ -59,12 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   _______,                       KC_SPC,             _______,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_NUM] = LAYOUT_ansi_89(
-        KC_MS_BTN1,  RGB_TOG,  		RGB_HUD,  	   RGB_HUI,  	   RGB_SAD,  RGB_SAI,  RGB_VAD,   RGB_VAI,  RGB_RMOD,  RGB_MOD,   KC_NO,  	KC_NO,    KC_NO,   KC_NO,   _______,            _______,
-        _______,  	KC_MS_ACCEL0, KC_MS_ACCEL1,  KC_MS_ACCEL2,      KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_PSLS,   KC_PAST,  KC_PMNS,   KC_NO,   _______, _______,  _______,            _______,
-        _______,  	 _______,  		 KC_NO,        KC_MS_UP,  	    KC_NO,	  KC_NO,    KC_NO,     KC_P7,    KC_P8,     KC_P9,   KC_PPLS,   KC_NO,    KC_NO,    KC_NO,  _______,            _______,
-        _______,  	  KC_NO,   	   KC_MS_LEFT,    KC_MS_DOWN,   KC_MS_RIGHT,  KC_NO,    KC_NO,     KC_P4,    KC_P5,     KC_P6,   KC_PPLS,   KC_NO,    KC_NO,            _______,            _______,
-        _______,  	  KC_NO,             		    KC_NO,   	    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_P0,     KC_P1,    KC_P2,    KC_P3,   KC_PENT,  KC_NO,    KC_NO,   _______,
-        _______,  	 _______,  		_______,       		     	   KC_NUM,   _______,  _______,                         KC_P0,             KC_PDOT,                     _______,  _______,  _______),
+        KC_MS_BTN1,   RGB_TOG,  		RGB_HUD,  	   RGB_HUI,  	   RGB_SAD,    RGB_SAI,  RGB_VAD,   RGB_VAI,  RGB_RMOD,  RGB_MOD,   KC_NO,  	KC_NO,    KC_NO,   KC_NO,   _______,            _______,
+        KC_MS_ACCEL0,  KC_NO,        KC_NO,         KC_NO,        KC_NO,      KC_NO,    KC_NO,     KC_NO,   KC_PSLS,   KC_PAST,  KC_PMNS,   KC_NO,   _______, _______,  _______,            _______,
+        KC_MS_ACCEL1, _______,    	 KC_NO,        KC_MS_UP,  	  KC_NO,	    KC_NO,    KC_NO,     KC_P7,    KC_P8,     KC_P9,   KC_PPLS,   KC_NO,    KC_NO,    KC_NO,  _______,            _______,
+        KC_MS_ACCEL2,  KC_NO,   	 KC_MS_LEFT,    KC_MS_DOWN,   KC_MS_RIGHT,  KC_NO,    KC_NO,     KC_P4,    KC_P5,     KC_P6,   KC_PPLS,   KC_NO,    KC_NO,            _______,            _______,
+         KC_NO,   	  _______,            		      KC_NO,   	    KC_NO,      KC_NO,    KC_NO,     KC_NO,    KC_P0,     KC_P1,    KC_P2,    KC_P3,   KC_PENT,  KC_NO,    KC_NO,   _______,
+        KC_NUM,   	  _______,  		_______,       		     	     _______,    _______,  _______,                         KC_P0,             KC_PDOT,                     _______,  _______,  _______),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
@@ -76,6 +76,44 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif // ENCODER_MAP_ENABLE
 
+// PARTY_MODE LIGHTS
+// trying moving things here from rg_matrix_user.inc to see if it compiles
+
+static HSV party_math(HSV hsv, uint8_t i, uint8_t time) {
+    hsv.h = g_led_config.point[i].x - time;
+    return hsv;
+}
+
+bool party_effect_i(effect_params_t* params, HSV effect_func) {
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+    
+    HSV hsv  = rgb_matrix_config.hsv;
+    RGB rgb1 = rgb_matrix_hsv_to_rgb(hsv);
+
+    uint8_t time = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed / 4, 1));
+    
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t i = g_led_config.matrix_co[row][col];
+
+            if (i >= led_min && i < led_max && i != NO_LED && HAS_FLAGS(g_led_config.flags[i], 0x04) &&
+            keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                RGB_MATRIX_TEST_LED_FLAGS();
+                RGB rgb = rgb_matrix_hsv_to_rgb(effect_func(rgb_matrix_config.hsv, i, time));
+                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+            }
+            if (!HAS_FLAGS(g_led_config.flags[i], 0x04)) {
+                rgb_matrix_set_color(i, rgb1.r, rgb1.g, rgb1.b);
+            }
+        }
+    }
+    return rgb_matrix_check_finished_leds(led_max);
+}
+
+bool party_keylgihts(effect_params_t* params) {
+    return party_effect_i(params, &party_math);
+}
+
 // LIGHTS PER LAYER
 // Modded from the link below to match/use Matrix effects
 // From https://www.reddit.com/r/olkb/comments/e0hurb/comment/fawrcem/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
@@ -84,38 +122,37 @@ void eeconfig_init_user(void) {  // EEPROM is getting reset!
   // use the non noeeprom versions, to write these values to EEPROM too
   rgb_matrix_enable(); // Enable RGB by default
   rgb_matrix_sethsv(HSV_PURPLE);  // Set it to teal by default
-  rgb_matrix_set_val(128); // Set it to half brighness
   rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE); // set to breathing by default
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  uint8_t rgbModelast rgb_matrix_get_mode();
-  uint8_t rgbHSVlast rgb_natrix_get_hsv();
+  uint8_t rgbModelast = rgb_matrix_get_mode();
+  HSV rgbHSVlast = rgb_matrix_get_hsv();
   switch(biton32(state)) {
   case 1:
     // tealish
     rgb_matrix_enable_noeeprom();
     rgb_matrix_mode_noeeprom(rgbModelast);
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast+56);
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+56,rgbHSVlast.s,rgbHSVlast.v);
     break;
   case 2:
     // greenish
     rgb_matrix_enable_noeeprom();	
     rgb_matrix_mode_noeeprom(rgbModelast);
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast)+112;
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+112,rgbHSVlast.s,rgbHSVlast.v);
     break;
   case 3:
     // orangeish
     rgb_matrix_enable_noeeprom();
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast);
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_party_keylights);
-
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h,rgbHSVlast.s,rgbHSVlast.v);
+    //rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_party_keylights);
+    party_keylights(effect_params_t* params);
     break;
   default:
     // if not touched, purpleish
     //If enabled, set white
     if (rgb_matrix_is_enabled()) {
-      rgb_matrix_sethsv_noeeprom(rgbHSVlast+168);
+      rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+168,rgbHSVlast.s,rgbHSVlast.v);
 	  } else { //Otherwise go back to disabled
 		  rgb_matrix_disable_noeeprom();
 	  }
