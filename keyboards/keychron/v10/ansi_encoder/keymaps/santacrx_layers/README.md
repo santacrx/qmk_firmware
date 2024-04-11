15fa751b9e4600b0d9def40c3ad7d06897369985
santacrx <santacrx@gmail.com>
2024-04-11 13:12:49 -0700

    hue change and persistantcy works, but not as intended.\n-num is not changing colors.\n-hue is constantly changing

--- keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_layers/README.md
+++ keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_layers/README.md
@@ -9,9 +9,12 @@ This is my customization for my v10 to maximize the capabilities plus visuals I 
   * _Numpad and mouse
 * Each layer has its own color scheme, adapted from [here]{https://www.reddit.com/r/olkb/comments/e0hurb/comment/fawrcem/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button}
 * Added some RGB_MATRIX modes that were not turned on by default
+* Trying to add colored functionality
 
 ## Worflow / Desired Features / Bugs
 - [x] Commit of first working version (2024-04-05)
-- [x] Change base color to other than white 
-- [x] Build a way to keep lightning changes made in L3 stick for L0 and not reset to the default
-- [ ] Party mode caps lock or numpad as seen [here]{http://daedalusrising.com/2019/10/07/the-massdrop-alt-custom-animations-and-more/}
+- [x] Change base color to other than white (2024-04-10)
+- [x] Build a way to keep lightning changes made in L3 stick for L0 and not reset to the default (2024-04-10)
+- [-] *MOVED TO SEPARATE DEV* Party mode caps lock or numpad as seen [here]{http://daedalusrising.com/2019/10/07/the-massdrop-alt-custom-animations-and-more/}
+- [ ] Correct bug where _NUM doesn't change hue
+- [ ] Make _NUM only light up actual keycodes
--- keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_layers/keymap.c
+++ keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_layers/keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN1] = LAYOUT_ansi_89(
         KC_MUTE,  KC_CALC,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_PGDN,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_PGUP,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_PGDN,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                      _______,  _______,  _______),
 
@@ -59,12 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,   _______,                       KC_SPC,             _______,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_NUM] = LAYOUT_ansi_89(
-        KC_MS_BTN1,  RGB_TOG,  		RGB_HUD,  	   RGB_HUI,  	   RGB_SAD,  RGB_SAI,  RGB_VAD,   RGB_VAI,  RGB_RMOD,  RGB_MOD,   KC_NO,  	KC_NO,    KC_NO,   KC_NO,   _______,            _______,
-        _______,  	KC_MS_ACCEL0, KC_MS_ACCEL1,  KC_MS_ACCEL2,      KC_NO,    KC_NO,    KC_NO,     KC_NO,   KC_PSLS,   KC_PAST,  KC_PMNS,   KC_NO,   _______, _______,  _______,            _______,
-        _______,  	 _______,  		 KC_NO,        KC_MS_UP,  	    KC_NO,	  KC_NO,    KC_NO,     KC_P7,    KC_P8,     KC_P9,   KC_PPLS,   KC_NO,    KC_NO,    KC_NO,  _______,            _______,
-        _______,  	  KC_NO,   	   KC_MS_LEFT,    KC_MS_DOWN,   KC_MS_RIGHT,  KC_NO,    KC_NO,     KC_P4,    KC_P5,     KC_P6,   KC_PPLS,   KC_NO,    KC_NO,            _______,            _______,
-        _______,  	  KC_NO,             		    KC_NO,   	    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_P0,     KC_P1,    KC_P2,    KC_P3,   KC_PENT,  KC_NO,    KC_NO,   _______,
-        _______,  	 _______,  		_______,       		     	   KC_NUM,   _______,  _______,                         KC_P0,             KC_PDOT,                     _______,  _______,  _______),
+        KC_MS_BTN1,   RGB_TOG,  		RGB_HUD,  	   RGB_HUI,  	   RGB_SAD,    RGB_SAI,  RGB_VAD,   RGB_VAI,  RGB_RMOD,  RGB_MOD,   KC_NO,  	KC_NO,    KC_NO,   KC_NO,   _______,            _______,
+        KC_MS_ACCEL0,  KC_NO, 	     KC_NO,         KC_NO,        KC_NO,      KC_NO,    KC_NO,     KC_NO,   KC_PSLS,   KC_PAST,  KC_PMNS,   KC_NO,   _______, _______,  _______,            _______,
+        KC_MS_ACCEL1, _______,    	 KC_NO,        KC_MS_UP,  	  KC_NO,	    KC_NO,    KC_NO,     KC_P7,    KC_P8,     KC_P9,   KC_PPLS,   KC_NO,    KC_NO,    KC_NO,  _______,            _______,
+        KC_MS_ACCEL2,  KC_NO,   	 KC_MS_LEFT,    KC_MS_DOWN,   KC_MS_RIGHT,  KC_NO,    KC_NO,     KC_P4,    KC_P5,     KC_P6,   KC_PPLS,   KC_NO,    KC_NO,            _______,            _______,
+         KC_NO,   	  _______,            		      KC_NO,   	    KC_NO,      KC_NO,    KC_NO,     KC_NO,    KC_P0,     KC_P1,    KC_P2,    KC_P3,   KC_PENT,  KC_NO,    KC_NO,   _______,
+        KC_NUM,   	  _______,  		_______,       		     	     _______,    _______,  _______,                         KC_P0,             KC_PDOT,                     _______,  _______,  _______),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
@@ -84,36 +84,35 @@ void eeconfig_init_user(void) {  // EEPROM is getting reset!
   // use the non noeeprom versions, to write these values to EEPROM too
   rgb_matrix_enable(); // Enable RGB by default
   rgb_matrix_sethsv(HSV_PURPLE);  // Set it to teal by default
-  rgb_matrix_set_val(128); // Set it to half brighness
   rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE); // set to breathing by default
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  uint8_t rgbModelast rgb_matrix_get_mode();
-  uint8_t rgbHSVlast rgb_natrix_get_hsv();
+  uint8_t rgbModelast = rgb_matrix_get_mode();
+  HSV rgbHSVlast = rgb_matrix_get_hsv();
   switch(biton32(state)) {
   case 1:
     // tealish
     rgb_matrix_enable_noeeprom();
     rgb_matrix_mode_noeeprom(rgbModelast);
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast+56);
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+56,rgbHSVlast.s,rgbHSVlast.v);
     break;
   case 2:
     // greenish
     rgb_matrix_enable_noeeprom();	
     rgb_matrix_mode_noeeprom(rgbModelast);
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast)+112;
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+112,rgbHSVlast.s,rgbHSVlast.v);
     break;
   case 3:
     // orangeish
     rgb_matrix_enable_noeeprom();
-    rgb_matrix_sethsv_noeeprom(rgbHSVlast);
+    rgb_matrix_sethsv_noeeprom(rgbHSVlast.h,rgbHSVlast.s,rgbHSVlast.v);
     break;
   default:
     // if not touched, purpleish
     //If enabled, set white
     if (rgb_matrix_is_enabled()) {
-      rgb_matrix_sethsv_noeeprom(rgbHSVlast+168);
+      rgb_matrix_sethsv_noeeprom(rgbHSVlast.h+168,rgbHSVlast.s,rgbHSVlast.v);
 	  } else { //Otherwise go back to disabled
 		  rgb_matrix_disable_noeeprom();
 	  }
