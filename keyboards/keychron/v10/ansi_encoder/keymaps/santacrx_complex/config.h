33041266a3e7c1b2039402b24ca9023ef9403046
santacrx <santacrx@gmail.com>
2025-05-08 15:07:22 -0700

    commented out matrix scan rate

--- keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_complex/config.h
+++ keyboards/keychron/v10/ansi_encoder/keymaps/santacrx_complex/config.h
@@ -3,3 +3,4 @@
 #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN  // Static gradient top to bottom, speed controls how much gradient changes
 #define ENABLE_RGB_MATRIX_SOLID_REACTIVE	// Static single hue, pulses keys hit to shifted hue then fades to current hue
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8 // Increase layer count to 8, because why not
+//#define DEBUG_MATRIX_SCAN_RATE
